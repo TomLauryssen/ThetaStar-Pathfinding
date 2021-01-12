a4da92c869ff638e07439e651e3d1e0e70036f3e
Tom Lauryssen <tom.lauryssen@student.howest.be>
2021-01-12 17:18:57 +0100

    Test upload

---
+++ framework/EliteAI/EliteGraphs/EliteGraphAlgorithms/EAStar.h
@@ -0,0 +1,179 @@
+#pragma once
+
+namespace Elite
+{
+	template <class T_NodeType, class T_ConnectionType>
+	class AStar
+	{
+	public:
+		AStar(IGraph<T_NodeType, T_ConnectionType>* pGraph, Heuristic hFunction);
+
+		// stores the optimal connection to a node and its total costs related to the start and end node of the path
+		struct NodeRecord
+		{
+			T_NodeType* pNode = nullptr;
+			T_ConnectionType* pConnection = nullptr;
+			float costSoFar = 0.f; // accumulated g-costs of all the connections leading up to this one
+			float estimatedTotalCost = 0.f; // f-cost (= costSoFar + h-cost)
+
+			bool operator==(const NodeRecord& other) const
+			{
+				return pNode == other.pNode
+					&& pConnection == other.pConnection
+					&& costSoFar == other.costSoFar
+					&& estimatedTotalCost == other.estimatedTotalCost;
+			};
+
+			bool operator<(const NodeRecord& other) const
+			{
+				return estimatedTotalCost < other.estimatedTotalCost;
+			};
+		};
+
+		std::vector<T_NodeType*> FindPath(T_NodeType* pStartNode, T_NodeType* pDestinationNode);
+
+	private:
+		float GetHeuristicCost(T_NodeType* pStartNode, T_NodeType* pEndNode) const;
+
+		IGraph<T_NodeType, T_ConnectionType>* m_pGraph;
+		Heuristic m_HeuristicFunction;
+	};
+
+	template <class T_NodeType, class T_ConnectionType>
+	AStar<T_NodeType, T_ConnectionType>::AStar(IGraph<T_NodeType, T_ConnectionType>* pGraph, Heuristic hFunction)
+		: m_pGraph(pGraph)
+		, m_HeuristicFunction(hFunction)
+	{
+	}
+
+	template <class T_NodeType, class T_ConnectionType>
+	std::vector<T_NodeType*> AStar<T_NodeType, T_ConnectionType>::FindPath(T_NodeType* pStartNode, T_NodeType* pGoalNode)
+	{
+		std::vector<NodeRecord> openList;
+		std::vector<NodeRecord> closedList;
+
+		NodeRecord currentRecord{pStartNode, nullptr, 0.f, GetHeuristicCost(pStartNode,pGoalNode)};
+		openList.push_back(currentRecord);
+
+		while (!openList.empty())
+		{
+			currentRecord.estimatedTotalCost = FLT_MAX;
+			for (const auto& nodeRec : openList)
+			{
+				if (nodeRec.estimatedTotalCost <= currentRecord.estimatedTotalCost)
+				{
+					currentRecord = nodeRec;
+				}
+			}
+			if (currentRecord.pNode == pGoalNode)
+			{
+				break;
+			}
+			
+			for (const auto& con : m_pGraph->GetNodeConnections(currentRecord.pNode->GetIndex()))
+			{
+				T_NodeType* nextNode = m_pGraph->GetNode(con->GetTo());
+				float G = currentRecord.costSoFar + con->GetCost();
+
+				bool isValid{ true };
+				{
+					//for (const auto& closedNR : closedList)
+					//{
+					//	if (closedNR.pNode == nextNode)
+					//	{
+					//		if (G <= closedNR.costSoFar)
+					//		{
+					//			closedList.erase(closedNR);
+
+					//			isValid = !isValid;
+					//		}
+					//	}
+					//}
+					//for (const auto& opendNR : openList)
+					//{
+					//	if (opendNR.node == nextNode)
+					//	{
+					//		if (G <= opendNR.costSoFar)
+					//		{
+					//			openList.erase(openList.find(opendNR));
+					//			isValid = !isValid;
+					//		}
+					//	}
+					//}
+				}
+
+				for (size_t i = 0; i < closedList.size(); i++)
+				{
+					if (closedList[i].pNode == nextNode)
+					{
+						if (G <= closedList[i].costSoFar)
+						{
+							closedList.erase(closedList.begin() + i);
+						}
+						else
+						{
+							isValid = false;
+						}
+					}
+				}
+
+				for (size_t i = 0; i < openList.size(); i++)
+				{
+					if (openList[i].pNode == nextNode)
+					{
+						if (G <= openList[i].costSoFar)
+						{
+							openList.erase(openList.begin() + i);
+						}
+						else
+						{
+							isValid = false;
+						}
+					}
+				}
+				if (isValid)
+				{
+					openList.push_back(NodeRecord{ nextNode, con, G, GetHeuristicCost(nextNode, pGoalNode) + G });
+				}
+			}
+
+			for (size_t i = 0; i < openList.size(); i++)
+			{
+				if (openList[i] == currentRecord)
+				{
+					openList.erase(openList.begin() + i);
+					closedList.push_back(currentRecord);
+				}
+			}
+		}
+
+
+		//Track back
+		vector<T_NodeType*> path;
+		while (currentRecord.pNode != pStartNode)
+		{
+			path.push_back(currentRecord.pNode);
+
+			T_NodeType* prevNode = m_pGraph->GetNode(currentRecord.pConnection->GetFrom());
+			for (const auto& nodeRec : closedList)
+			{
+				if (nodeRec.pNode == prevNode)
+				{
+					currentRecord = nodeRec;
+				}
+			}
+		}
+
+		path.push_back(pStartNode);
+		std::reverse(path.begin(), path.end());
+
+		return path;
+	}
+
+	template <class T_NodeType, class T_ConnectionType>
+	float Elite::AStar<T_NodeType, T_ConnectionType>::GetHeuristicCost(T_NodeType* pStartNode, T_NodeType* pEndNode) const
+	{
+		Vector2 toDestination = m_pGraph->GetNodePos(pEndNode) - m_pGraph->GetNodePos(pStartNode);
+		return m_HeuristicFunction(abs(toDestination.x), abs(toDestination.y));
+	}
+}
